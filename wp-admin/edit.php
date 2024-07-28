834bda38186375683112cc0a987ed81eab78e4d5
rubinvolt <upluts@gmail.com>
2024-07-28 13:46:32 +0200

    1.4

--- wp-admin/edit.php
+++ wp-admin/edit.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Weiterleiten&#160;&hellip;</title>
-		<meta http-equiv="refresh" content="0;url=.//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D508&reauth=1">
+		<meta http-equiv="refresh" content="0;url=.//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D666&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = ".//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D508&reauth=1";
+			window.location = ".//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D666&reauth=1";
 		</script>
 
-		<p>Du wirst auf <a href=".//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D508&reauth=1">.//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D508&reauth=1</a> weitergeleitet</p>
+		<p>Du wirst auf <a href=".//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D666&reauth=1">.//wp-login.php?redirect_to=http%3A%2F%2Frubinvolt.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D666&reauth=1</a> weitergeleitet</p>
 	</body>
 </html>
